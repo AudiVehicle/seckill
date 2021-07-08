929e5306bff2a6a12118b216797019f0a490d7b7
王全洲 <wangquanzhou@t4f-mbp-wqz.local>
2021-07-08 13:10:15 +0800

    增加刷新redis数据到mysql的路评级

--- src/main/java/com/audi/seckill/product/service/impl/ProductServiceImpl.java
+++ src/main/java/com/audi/seckill/product/service/impl/ProductServiceImpl.java
@@ -1,12 +1,15 @@
 package com.audi.seckill.product.service.impl;
 
 import com.audi.seckill.product.dao.ProductDao;
+import com.audi.seckill.product.entity.Product;
 import com.audi.seckill.product.service.ProductService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scripting.ScriptSource;
 import org.springframework.scripting.support.ResourceScriptSource;
 import org.springframework.stereotype.Service;
@@ -54,6 +57,28 @@ public class ProductServiceImpl implements ProductService {
             log.info("未能成功实现商品抢购");
             return Boolean.FALSE;
         }
+    }
+
+    /**
+     * 定时获取redis的商品余量刷新到数据库
+     */
+    @Scheduled(initialDelay = 10 * 1000, fixedRate = 2 * 1000)
+    public void refresh() {
+        log.info("开始刷新redis商品余量信息到数据库");
+
+        Integer productId = 1111;
+        Integer rest = (Integer) redisTemplate.opsForValue().get(productId);
+
+        log.info("从redis获取到商品 {} 的余量为 {}", productId, rest);
+
+        Product product = new Product();
+        product.setRest(rest);
+        LambdaQueryWrapper<Product> wrapper = new LambdaQueryWrapper<Product>().eq(Product::getProductId, productId);
+
+        productDao.update(product, wrapper);
+
+
+        log.info("结束刷新redis商品余量信息到数据库");
     }
 
     private void getRedisScript() {
